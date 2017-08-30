d055c8689f5aedd0172de7556e8202339ec1cc0a
Rafael Lima <rafaelccomp@gmail.com>
2017-08-30 11:35:45 -0300

    If then else

--- aula02/areatri.c
+++ aula02/areatri.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 void q2(){
-
-  float base, altura, area_tri;
-  printf("Entre com a base e altura do triângulo:");
-  scanf("%f %f", &base, &altura);
-  area_tri = 0.5*base*altura;
-  printf("A área do tri eh: %.2f de base %.2f e altura %.2f\n", area_tri, base, altura);
+  /* Função que verifica se um número é maior que 10.
+  */
+  float num;
+  printf("Entre com o número: ");
+  scanf("%f", &num);
+  if(num>10){
+    printf("Maior que 10");
+  }
+  /*else
+    printf("Menor ou igual a 10");*/
 }
 void q3(){
 
@@ -17,7 +21,7 @@ printf("A área do tri eh: %.2f de base %.2f e altura %.2f\n", area_tri, base, a
 }
 
 int main(int argc, char const *argv[]) {
-  //q2();
-  q3();
+  q2();
+  //q3();
   return 0;
 }
